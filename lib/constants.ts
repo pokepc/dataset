f657a2371c715afe2f648c9fac688db6692e6db9
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2026-03-03 05:56:16 +0100

    add gen10 games and fix some dates and game platforms

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,2 @@
-export const POKEPC_LATEST_GENERATION = 9
-export const POKEPC_UNRELEASED_GAMES = ['champions']
-export const POKEPC_SWITCH_GAME_IDS = ['lgpe', 'home', 'swsh', 'bdsp', 'la', 'sv', 'lza']
-export const POKEPC_SWITCH2_GAME_IDS = ['lgpe', 'home', 'swsh', 'bdsp', 'la', 'sv', 'lza']
+export const POKEPC_LATEST_GENERATION = 10
+export const POKEPC_UNRELEASED_GAMES = ['champions', 'wiwa', 'wiwa-wi', 'wiwa-wa']
