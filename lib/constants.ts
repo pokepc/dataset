7564db2b9de854c5cf307bf55f15d71377c71fb7
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2026-04-15 12:47:00 +0200

    fix champions release date

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 export const POKEPC_LATEST_GENERATION = 10
-export const POKEPC_UNRELEASED_GAMES = ['champions', 'wiwa', 'wiwa-wi', 'wiwa-wa']
+export const POKEPC_UNRELEASED_GAMES = ['wiwa', 'wiwa-wi', 'wiwa-wa']
 
 export const NON_CANONICAL_POKEMON_IDS = [
   'professortangrowth',
